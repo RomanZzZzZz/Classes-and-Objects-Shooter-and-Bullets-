718dd0d563054944596727b93835ae94ee4aa399
Roman Zain <zainygamesinc1@gmail.com>
2026-04-29 16:31:35 -0500

    wip

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def playing_area():
     pen.end_fill()
     
 class Player(Turtle):
-    def __init__(self, x, y, color, screen, right_key, left_key):
+    def __init__(self, x, y, color, screen, right_key, left_key, fire_key):
         super().__init__()
         self.ht()
         self.speed(0)
@@ -28,10 +28,12 @@ class Player(Turtle):
         self.setheading(90)
         self.shape("turtle")
         self.bullets = []
+        self.color = color
         self.alive = True
         self.st()
         screen.onkeypress(self.turn_left, left_key)
         screen.onkeypress(self.turn_right, right_key)
+        screen.onkey(self.fire, fire_key)
 
     def turn_left(self):
         self.left(10)
@@ -46,6 +48,14 @@ class Player(Turtle):
         if self.ycor() > 230 or self.ycor() < -230:
             self.setheading(-self.heading())
 
+    def fire(self):
+        self.bullets.append(Bullet(self))
+        pass
+
+
+
+
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(520,520)
@@ -55,8 +65,8 @@ screen.listen()
 
 playing_area()
 
-p1 = Player(-100, 0, "red",screen, "d", "a")
-p2 = Player(100,0,"blue",screen, "Right","Left")
+p1 = Player(-100, 0, "red",screen, "d", "a", 'w')
+p2 = Player(100,0,"blue",screen, "Right","Left", 'Up')
 
 while p1.alive and p2.alive:
     p1.move()
